f2f0e2367d231d4d2597bd02f8e8f636e6faf27e
정수진 <sssjsjj@naver.com>
2019-10-07 09:09:10 +0900

    poll es6로 바꿔보기 중

--- 190924_poll_test/polls.js
+++ 190924_poll_test/polls.js
@@ -1,40 +1,47 @@
-// //virtual data
-// const optionType1 = ["매우 아니다", "아니다", "보통이다", "그렇다", "매우 그렇다"];
-// const questions = [
-//   {
-//     "index" : 1,
-//     "title" : "강의계획이 체계적이었으며, 학사일정에 맞추어 안내하였다.",
-//     "options" : optionType1
-//   },
-//   {
-//     "index" : 2,
-//     "title" : "강의계획이 체계적이었으며, 학사일정에 맞추어 안내하였다.",
-//     "options" : optionType1
-//   },
-//   {
-//     "index" : 3,
-//     "title" : "강의계획이 체계적이었으며, 학사일정에 맞추어 안내하였다.",
-//     "options" : optionType1
-//   }
-// ]
+//virtual data
 
 const polls = [
+  [1, 5, 2], [4, 3, 1], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 1], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [3, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [3, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [3, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [3, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [3, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [3, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 5], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
+  [1, 5, 2], [4, 3, 3], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
   [1, 5, 2], [4, 3, 1], [5, 4, 3], [1, 5, 4], [2, 3, 1], [5, 4, 3], 
   [1, 3, 2], [3, 5, 1], [4, 3, 2], [5, 1, 4], [2, 3, 1], [5, 4, 2], 
   [2, 3, 2], [4, 1, 2], [3, 1, 5], [1, 2, 5], [4, 3, 1], [5, 4, 3], 
   [2, 1, 5], [4, 3, 1], [2, 5, 1], [2, 4, 3], [1, 5, 2], [4, 3, 2], 
-  [1, 5, 2], [1, 2, 3], [5, 4, 1], [5, 2, 4], [3, 1, 3], [1, 3, 5]
+  [1, 5, 2], [1, 2, 3], [5, 4, 1], [5, 2, 4], [3, 1, 3], [1, 3, 5],
+  [1, 5, 2], [1, 2, 3], [5, 4, 1], [5, 2, 4], [3, 1, 3], [1, 3, 5],
+  [1, 5, 2], [1, 2, 3], [5, 4, 1], [5, 2, 4], [3, 1, 3], [1, 3, 5],
+  [1, 5, 2], [4, 3, 1], [5, 4, 3], [1, 4, 3], [1, 2, 1], [2, 2, 2],
+  [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1],
+  [1, 1, 1],
 ];
 
 let graphs = [];
 const questions = document.querySelectorAll("[data-js=question]"), //문항 갯수
       graphAreaSample = document.querySelectorAll(".list_poll_graph")[0], //그래프 막대 영역 (사이즈 측정 위해 예시 1개만)
+      options = graphAreaSample.children, //문항 선택지 갯수
       optionLength = graphAreaSample.children.length, //문항 선택지 갯수
       graphIndexes = document.querySelectorAll(".index_graph"), //그래프 인덱스
-      indexGap = 2;//그래프 인덱스 간격 설정
-      
+      indexGap = 5;
 
-questions.forEach(function(question, index){
+questions.forEach((question, index) => {
   graphs[index] = {};
   graphs[index].answers = [];
   graphs[index].scores = [];
@@ -42,22 +49,32 @@ questions.forEach(function(question, index){
   graphs[index].indexNum = 0;
   graphs[index].average = 0;
   graphs[index].scoreSum = 0;
+  
 /* 설문조사 데이터 문항별 수치를 구하기 위한 과정 - 1
   문항별 수치들을 1개의 스트링으로 묶어 배열에 저장
 */
   let strAllPolls = [];
   strAllPolls[index] = "";
-  for(let poll in polls){
-    strAllPolls[index] += (polls[poll][index]);
+  for(const poll of polls){
+    strAllPolls[index] += (poll[index]);
   }
 
 /* 설문조사 데이터 문항별 수치를 구하기 위한 과정 - 2
   저장한 스트링에 해당 옵션이 몇번 나오는지 구하기 위해 
   해당 지점을 잘라 -1 한다.
-  */
-
+*/
   for (let y = 0; y < optionLength; y++ ){
     graphs[index].answers[y] = (strAllPolls[index].split(y + 1).length - 1);
+  }
+
+  for(const option in options){
+    // graphs[index].answers[y] = (strAllPolls[index].split(y + 1).length - 1);
+    // console.log(options.indexOf(option));
+    // console.log(options[option]);
+    // console.log(option);    
+    console.log(options.length);    
+    console.log(options.item);    
+    console.log(options.itemnamedItem);    
   }
 
 /* 문항별 최대 수치
@@ -68,7 +85,7 @@ questions.forEach(function(question, index){
 
   //graph vertical index add
   for(let y = 0; y <= graphs[index].indexNum; y++){
-    graphIndexes[index].innerHTML += 				"<li class=\"item\"><span class=\"txt_num\">"+  (indexGap * y) + "</span></li>"
+    graphIndexes[index].innerHTML += "				<li class=\"item\"><span class=\"txt_num\">"+  (indexGap * y) + "</span></li>";
   }
 
   //graph horizontal bar
@@ -76,7 +93,7 @@ questions.forEach(function(question, index){
         graphBars = graphBarArea.querySelectorAll(".bar_graph");
 
   for(let y = 0; y < optionLength; y++){
-    graphBars[y].style.width = Math.round(graphs[index].answers[y] / ( graphs[index].indexNum * 2) * 100) + "%";
+    graphBars[y].style.width = graphs[index].answers[y] / ( graphs[index].indexNum * indexGap) * 100 + "%";
     graphBars[y].querySelector(".txt_num").innerHTML = graphs[index].answers[y];
 
     // Score by Selection
@@ -84,7 +101,7 @@ questions.forEach(function(question, index){
   } 
 
   // Sum all scores by selection
-  graphs[index].scoreSum = graphs[index].scores.reduce(function(total, num){
+  graphs[index].scoreSum = graphs[index].scores.reduce((total, num) => {
     return total + num;
   });
 
@@ -95,7 +112,5 @@ questions.forEach(function(question, index){
   const averages = question.querySelector(".average");
   averages.innerHTML = graphs[index].average.toFixed(2);
 });
-
-
 
 console.log(graphs);
