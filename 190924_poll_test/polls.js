0e2ff05a44e80cf4818fd6c5781adf2f6f62ff8d
정수진 <sssjsjj@naver.com>
2019-10-18 16:49:31 +0900

    poll스크립트 es6도전, text rolling 신규 과제 추가

--- 190924_poll_test/polls.js
+++ 190924_poll_test/polls.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 //virtual data
 
 const polls = [
@@ -36,8 +37,8 @@ const polls = [
 let graphs = [];
 const questions = document.querySelectorAll("[data-js=question]"), //문항 갯수
       graphAreaSample = document.querySelectorAll(".list_poll_graph")[0], //그래프 막대 영역 (사이즈 측정 위해 예시 1개만)
-      options = graphAreaSample.children, //문항 선택지 갯수
-      optionLength = graphAreaSample.children.length, //문항 선택지 갯수
+      graphAreas = document.querySelectorAll(".list_poll_graph"), //그래프 막대 영역 (사이즈 측정 위해 예시 1개만)
+      optionLength = options.length, //문항 선택지 갯수
       graphIndexes = document.querySelectorAll(".index_graph"), //그래프 인덱스
       indexGap = 5;
 
@@ -63,19 +64,10 @@ questions.forEach((question, index) => {
   저장한 스트링에 해당 옵션이 몇번 나오는지 구하기 위해 
   해당 지점을 잘라 -1 한다.
 */
-  for (let y = 0; y < optionLength; y++ ){
-    graphs[index].answers[y] = (strAllPolls[index].split(y + 1).length - 1);
-  }
-
-  for(const option in options){
-    // graphs[index].answers[y] = (strAllPolls[index].split(y + 1).length - 1);
-    // console.log(options.indexOf(option));
-    // console.log(options[option]);
-    // console.log(option);    
-    console.log(options.length);    
-    console.log(options.item);    
-    console.log(options.itemnamedItem);    
-  }
+  graphAreas.forEach((graphArea, i) => {
+    options = Array.from(graphArea.children); //문항 선택지
+    graphs[index].answers[i] = (strAllPolls[index].split(i + 1).length - 1);
+  }); 
 
 /* 문항별 최대 수치
   문항별 최대 수치에 따라 생성되는 그래프 인덱스 갯수
@@ -84,21 +76,19 @@ questions.forEach((question, index) => {
   graphs[index].indexNum = Math.ceil( graphs[index].maxValue / indexGap);
 
   //graph vertical index add
-  for(let y = 0; y <= graphs[index].indexNum; y++){
-    graphIndexes[index].innerHTML += "				<li class=\"item\"><span class=\"txt_num\">"+  (indexGap * y) + "</span></li>";
+  for(const i = 0; i <= graphs[index].indexNum; i++){
+    graphIndexes[index].innerHTML += "				<li class=\"item\"><span class=\"txt_num\">"+  (indexGap * i) + "</span></li>";
   }
 
   //graph horizontal bar
   const graphBarArea = question.querySelector(".list_poll_graph"),
         graphBars = graphBarArea.querySelectorAll(".bar_graph");
-
-  for(let y = 0; y < optionLength; y++){
-    graphBars[y].style.width = graphs[index].answers[y] / ( graphs[index].indexNum * indexGap) * 100 + "%";
-    graphBars[y].querySelector(".txt_num").innerHTML = graphs[index].answers[y];
-
+  options.forEach((opt, i) => {
+    graphBars[i].style.width = graphs[index].answers[i] / ( graphs[index].indexNum * indexGap) * 100 + "%";
+    graphBars[i].querySelector(".txt_num").innerHTML = graphs[index].answers[i];
     // Score by Selection
-    graphs[index].scores[y] = graphs[index].answers[y] * ( y * 25 );
-  } 
+    graphs[index].scores[i] = graphs[index].answers[i] * ( i * 25 );
+  }); 
 
   // Sum all scores by selection
   graphs[index].scoreSum = graphs[index].scores.reduce((total, num) => {
