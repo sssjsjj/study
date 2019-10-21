1790587403cfcdaa05ad84136bb34935c5360cb8
정수진 <sssjsjj@naver.com>
2019-10-21 19:22:55 +0900

    중간저장

--- 191018_text_rolling/text.js
+++ 191018_text_rolling/text.js
@@ -5,57 +5,61 @@ function log(code) {
 //  텍스트 롤링 
 function rollingText() {
   const $rollTxtArea = document.querySelectorAll("[data-text]"),
-    speed = 500,
-    _textPieces = [];
+    speed = 500;   
   let index = 0;
   for (const rollTxtArea of $rollTxtArea) {
     const text = rollTxtArea.dataset.text;
-
-    // 텍스트인 경우 쪼개서 배열에 넣어줘 
-      breakText(text, _textPieces);
-    
-    //숫자인 경우 순차적으로 그려줘
-    if(Number(text)) {
-      drawNumber(0, rollTxtArea, text);
-    }
-
-    // for(const piece of _textPieces[index]){
-    // }
-
+        
+    let fired = false,
+        fired1 = false;
+    window.addEventListener("load", () => {
+      //숫자인 경우 카운팅하면서 그려줘
+      if( rollTxtArea.offsetTop - 400 <= window.scrollY + 200 < rollTxtArea.offsetTop ){
+        rolling(text, rollTxtArea, fired, fired1);
+      }
+    });
+    window.addEventListener("scroll", () => {
+      //숫자인 경우 카운팅하면서 그려줘
+      if(window.scrollY + 200 >= rollTxtArea.offsetTop){
+        rolling(text, rollTxtArea, fired, fired1);  
+      }else if(document.querySelector("body").scrollHeight <  window.innerHeight + window.scrollY){
+        rolling(text, rollTxtArea, fired, fired1);  
+      }
+    });
     index++;
   }
 }
 rollingText();
 
-window.onscroll = function(){
-  log(window.scrollY);//10단위로 올리기
+function rolling(text , area, fired, fired1){
+  if(Number(text) && !fired) {
+    drawNumber(area, 0, text);
+    fired = true;
+  }else if(isNaN(Number(text)) && !fired1){
+    drawNaN(area, text, 0);        
+    fired1 = true;
+  }      
 }
 
-// 텍스트 쪼개서 배열에 넣는 함수 
-function breakText(text, arr) {
-  const letters = [],
-        textLength = text.length;
-  for (i = 0; i < textLength; i++){
-    letters.push(text.substring(i,i+1));
-  }  
-  arr.push(letters);
-}
-
-// 숫자인 경우 순차적으로 그려줘
-function drawNumber(startNum, area, limitNum) {
+// 카운팅 하면서 그려줘 ( 숫자 )
+function drawNumber(area, startNum, limitNum) {
   if(startNum <= limitNum){
     setTimeout(() => {
       area.textContent = startNum;
       startNum++;
-      drawNumber(startNum, area, limitNum);
+      drawNumber(area, startNum, limitNum);
     }, 1500 / limitNum);
-  }         
+  }           
 }
 
-//특정 영역에 갔을 때 함수 실행해 줘
-function startScrTop(target, func) {
-  if(window.scrollY > target.offset.top){
-
-    return false;
+// 한 글자씩 그려줘 ( NaN )
+function drawNaN(area, text, startNum) {
+  log(text.length);
+  if(startNum <= text.length){
+    setTimeout(() => {
+      area.textContent += text.substring(startNum,startNum+1);
+      startNum++;
+      drawNaN(area, text, startNum);
+    }, 800 / text.length);
   }
 }
