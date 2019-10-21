a0a3dc40d5414820d12b0157f4559ac0f32cd14b
JungSujin <sssjsjj@gmail.com>
2019-10-21 22:14:15 +0900

    무식하게함수복제되어있음. 정리 전

--- 191018_text_rolling/text.js
+++ 191018_text_rolling/text.js
@@ -14,16 +14,34 @@ function rollingText() {
         fired1 = false;
     window.addEventListener("load", () => {
       //숫자인 경우 카운팅하면서 그려줘
-      if( rollTxtArea.offsetTop - 400 <= window.scrollY + 200 < rollTxtArea.offsetTop ){
-        rolling(text, rollTxtArea, fired, fired1);
+      if( rollTxtArea.offsetTop - 200 <= window.scrollY + 200){
+        if(Number(text) && !fired) {
+          drawNumber(rollTxtArea, 0, text);
+          fired = true;
+        }else if(isNaN(Number(text)) && !fired1){
+          drawNaN(rollTxtArea, text, 0);        
+          fired1 = true;
+        }      
       }
     });
     window.addEventListener("scroll", () => {
       //숫자인 경우 카운팅하면서 그려줘
-      if(window.scrollY + 200 >= rollTxtArea.offsetTop){
-        rolling(text, rollTxtArea, fired, fired1);  
+      if(window.scrollY + 400 >= rollTxtArea.offsetTop){
+        if(Number(text) && !fired) {
+          drawNumber(rollTxtArea, 0, text);
+          fired = true;
+        }else if(isNaN(Number(text)) && !fired1){
+          drawNaN(rollTxtArea, text, 0);        
+          fired1 = true;
+        }      
       }else if(document.querySelector("body").scrollHeight <  window.innerHeight + window.scrollY){
-        rolling(text, rollTxtArea, fired, fired1);  
+        if(Number(text) && !fired) {
+          drawNumber(rollTxtArea, 0, text);
+          fired = true;
+        }else if(isNaN(Number(text)) && !fired1){
+          drawNaN(rollTxtArea, text, 0);        
+          fired1 = true;
+        }      
       }
     });
     index++;
@@ -31,15 +49,15 @@ function rollingText() {
 }
 rollingText();
 
-function rolling(text , area, fired, fired1){
-  if(Number(text) && !fired) {
-    drawNumber(area, 0, text);
-    fired = true;
-  }else if(isNaN(Number(text)) && !fired1){
-    drawNaN(area, text, 0);        
-    fired1 = true;
-  }      
-}
+// function rolling(text , area, fired, fired1){
+//   if(Number(text) && !fired) {
+//     drawNumber(area, 0, text);
+//     fired = true;
+//   }else if(isNaN(Number(text)) && !fired1){
+//     drawNaN(area, text, 0);        
+//     fired1 = true;
+//   }      
+// }
 
 // 카운팅 하면서 그려줘 ( 숫자 )
 function drawNumber(area, startNum, limitNum) {
