8e2c22f242ccb27427f439efbc17f2aa17210546
JungSujin <sssjsjj@gmail.com>
2019-10-22 01:07:23 +0900

    쪼꼼 더 다듬

--- 191018_text_rolling/text.js
+++ 191018_text_rolling/text.js
@@ -3,27 +3,24 @@ function log(code) {
 }
 
 //  텍스트 롤링 
-function rollingText() {  
-  const $rollTxtArea = document.querySelectorAll("[data-text]");
+function rollingText() {
+  const texts = document.querySelectorAll("[data-text]");
   let rTxts = [],
       i = 0;
 
-  for (const rollTxtArea of $rollTxtArea) {
-    const cont = rollTxtArea.dataset.text;
-
+  for (const text of texts) {
     rTxts[i] = {};
-    rTxts[i].elem = rollTxtArea;
-    rTxts[i].cont = cont;
-    rTxts[i].type = Number(cont) ? "number" : "NaN";
+    rTxts[i].elem = text;
+    rTxts[i].cont = text.dataset.text;
     rTxts[i].done = false;
 
     i++;
-  }  
+  }
   log(rTxts);
 
   for (const rTxt of rTxts) {
     let scrolling = false;
-        
+
     window.addEventListener("load", () => {
       rollTime(rTxt);
     });
@@ -32,8 +29,8 @@ function rollingText() {
       scrolling = true;
     });
 
-    setInterval( () => {
-      if( scrolling ) {
+    setInterval(() => {
+      if (scrolling) {
         scrolling = false;
         rollTime(rTxt);
       }
@@ -43,44 +40,44 @@ function rollingText() {
 }
 rollingText();
 
-function roll(rTxt){
-  if(Number(rTxt.cont) && !rTxt.done) {
+function roll(rTxt) {
+  if (Number(rTxt.cont) && !rTxt.done) {
     drawNumber(rTxt.elem, 0, rTxt.cont);
     rTxt.done = true;
-  }else if(isNaN(Number(rTxt.cont)) && !rTxt.done){
-    drawNaN(rTxt.elem, rTxt.cont, 0);        
+  } else if (isNaN(Number(rTxt.cont)) && !rTxt.done) {
+    drawNaN(rTxt.elem, rTxt.cont, 0);
     showNaN(rTxt.elem, rTxt.cont, 0);
     rTxt.done = true;
-  }   
+  }
 }
 
-function rollTime(rTxt){
+function rollTime(rTxt) {
   let winH = window.innerHeight,
-  winScrY = window.scrollY,
-  bodyScrH = document.querySelector("body").scrollHeight,
-  areaScrTop = rTxt.elem.offsetTop;
-  if(areaScrTop - winH/2 < winScrY && winScrY < areaScrTop - 20){
+      winScrY = window.scrollY,
+      bodyScrH = document.querySelector("body").scrollHeight,
+      areaScrTop = rTxt.elem.offsetTop;
+  if (areaScrTop - winH / 2 < winScrY && winScrY < areaScrTop - 20) {
     roll(rTxt);
-  }else if(bodyScrH - winH < areaScrTop && bodyScrH < winScrY + winH){
+  } else if (bodyScrH - winH < areaScrTop && bodyScrH < winScrY + winH) {
     roll(rTxt);
   }
 }
 
 // 숫자 카운팅 하면서 그려줘 ( Number )
 function drawNumber(area, startNum, limitNum) {
-  if(startNum <= limitNum){
+  if (startNum <= limitNum) {
     setTimeout(() => {
       area.textContent = startNum;
       startNum++;
       drawNumber(area, startNum, limitNum);
     }, 1500 / limitNum);
-  }           
+  }
 }
 
 // 한 글자씩 셋팅 ( NaN )
 function drawNaN(area, text, startNum) {
   // log(text.length);
-  if(startNum < text.length){
+  if (startNum < text.length) {
     area.innerHTML += `<span style="opacity:0;">${text.substring(startNum,startNum+1)}</span>`;
     startNum++;
     drawNaN(area, text, startNum);
@@ -88,8 +85,8 @@ function drawNaN(area, text, startNum) {
 }
 
 // 한 글자씩 show ( NaN )
-function showNaN(area, text, startNum){
-  if(startNum < text.length){
+function showNaN(area, text, startNum) {
+  if (startNum < text.length) {
     setTimeout(() => {
       area.querySelectorAll("span")[startNum].style = "opacity: 1";
       startNum++;
