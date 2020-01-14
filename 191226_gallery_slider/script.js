82ce1b9a6e798971b448b1f373756aaba8bcdf55
정수진 <sssjsjj@naver.com>
2020-01-14 18:11:58 +0900

    갤러리슬라이더기능도넣기전^^......정리된상태

--- 191226_gallery_slider/script.js
+++ 191226_gallery_slider/script.js
@@ -1,33 +1,32 @@
 /*jshint esversion: 6*/
 
-
-
-const btnLayerToggles = document.querySelectorAll("[data-role*='layertoggle']"),
-targetLayers = document.querySelectorAll("[data-role*='layertarget']");
-
-console.log(el_thisDataSet(targetLayers, "index", "0"))
-
-  btnLayerToggles.forEach(btnLayerToggle => {
-    btnLayerToggle.addEventListener("click", (e)=>{
-      let i = btnLayerToggle.dataset.index,
-      targetLayer = el_thisDataSet(targetLayers, "index", i);
-      if(hasClass(targetLayer, "open")){
-        removeClass(targetLayer, "open");
-      }else{
-        addClass(targetLayer, "open");
-      }
+/************
+  생성자 함수들
+************/
+//버튼 클릭 생성자 함수
+function ClickButtons(name) {
+  this.name = name;
+}
+ClickButtons.prototype.click = function(callback) {
+  this.name.forEach(el => {
+    el.addEventListener("click", ()=>{
+      callback(el);
     });
-  });
+  });    
+};
 
-
+/************
+  미니 함수들
+************/
+// 클래스명 추가
 function addClass(el, className){
-  el.className += " "+className;
+  el.className += " " + className;
 }
-
+// 클래스명 제거
 function removeClass(el, className){
-  el.className = el.className.replace(" "+className,"");
+  el.className = el.className.replace(" " + className,"");
 }
-
+// 클래스값 가지고 있는지 체크
 function hasClass(el, className){
   let arr = el.className.split(" ");
   if(arr.indexOf(className) > -1){
@@ -36,14 +35,30 @@ function hasClass(el, className){
     return false;
   }
 }
-
-
-function el_thisDataSet(els, dataSetName, dataSetValue){
-  let target;
+// 속성값 가지고 있는 엘리먼트
+function getElByAttr(els, attrName, attrValue){
+  let that;
   els.forEach(el => {
-    if(el.dataset[dataSetName] === dataSetValue){
-      target = el;
+    if(el.attributes[attrName].value === attrValue){
+      that = el;
     }
   });
-  return target;
+  return that;
 }
+
+/************
+  재료들로 DOM 만지기 
+************/
+const togLayerBtns = document.querySelectorAll("[data-role*='layertoggle']"),
+      togLayerBtn = new ClickButtons(togLayerBtns),
+      togLayers = document.querySelectorAll("[data-role*='layertarget']");
+
+togLayerBtn.click((el)=>{
+  let i = el.dataset.index,
+      thisLayer = getElByAttr(togLayers, "data-index", i);
+  if(hasClass(thisLayer, "open")){
+    removeClass(thisLayer, "open");
+  }else{
+    addClass(thisLayer, "open");
+  }
+});
